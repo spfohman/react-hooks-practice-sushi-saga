f839fb9a749bda8c66ff9656ebded2f2243a205b
+ <+>
2021-12-01 10:37:49 -0500

    commit tree 5898db957886563ec1e6c4c7d3c885d3554e9837

--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,17 @@
 import React from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer({ fourSushi, eatSushi, getSushi }) {
+  const sushiList = fourSushi
+    .filter((sushi) => !sushi.eaten)
+    .map((s) => {
+      return <Sushi key={s.id} sushi={s} eatSushi={eatSushi} />;
+    });
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushiList}
+      <MoreButton getSushi={getSushi} />
     </div>
   );
 }
