4b894685d27c7e890e02f9a8d446765d72903e37
+ <+>
2021-12-01 10:37:55 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [sushi, setSushi] = useState([]);
+  const [fourSushi, setFourSushi] = useState([]);
+  const [budget, setBudget] = useState(100);
+
+  useEffect(() => {
+    fetch(API)
+      .then((response) => response.json())
+      .then((data) => {
+        const allSushi = data.map((s) => {
+          return { ...s, eaten: false };
+        });
+
+        const four = allSushi.splice(0, 4);
+        setSushi(allSushi);
+        setFourSushi(four);
+      });
+  }, []);
+
+  const getSushi = () => {
+    const allSushi = sushi;
+    const four = sushi.splice(0, 4);
+    setSushi(allSushi);
+    setFourSushi(four);
+  };
+
+  const eatSushi = (e) => {
+    if (budget - parseInt(e.target.dataset.price) >= 0) {
+      const newSushi = fourSushi.map((s) =>
+        s.id == e.target.id ? { ...s, eaten: true } : s
+      );
+      setFourSushi(newSushi);
+      let newBudget = budget - parseInt(e.target.dataset.price);
+
+      setBudget(newBudget);
+    }
+  };
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer
+        fourSushi={fourSushi}
+        eatSushi={eatSushi}
+        getSushi={getSushi}
+      />
+      <Table fourSushi={fourSushi} budget={budget} />
     </div>
   );
 }
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-function Table({ plates = [] }) {
+function Table({ fourSushi, budget }) {
   // renders an empty plate for every element in the array
-  const emptyPlates = plates.map((_, index) => (
-    <div key={index} className="empty-plate" style={{ top: -7 * index }} />
-  ));
+  const emptyPlates = fourSushi
+    .filter((s) => s.eaten)
+    .map((s, index) => (
+      <div key={index} className="empty-plate" style={{ top: -7 * index }} />
+    ));
 
   return (
     <>
-      <h1 className="remaining">
-        You have: ${/* Give me how much money I have left */} remaining!
-      </h1>
+      <h1 className="remaining">You have: ${budget} remaining!</h1>
       <div className="table">
         <div className="stack">{emptyPlates}</div>
       </div>
